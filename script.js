b52010e9f3f6a590f1f7b878ae361bf79eae9bc1
ReThinkMoney <Phillipp.mh85@gmail.com>
2025-06-24 19:28:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const formData = new FormData(form);
-
+    
+// Nutzer informieren und Button deaktivieren
+    statusMessage.style.color = "black";
+    statusMessage.textContent = "⏳ Dein PDF wird generiert und versendet, dies kann bis zu 2 Minuten dauern. Bitte nicht erneut klicken.";
+    submitButton.disabled = true;
+    
     try {
         const res = await fetch("https://rethink-pdf-backend.onrender.com/send", {
             method: "POST",
@@ -26,4 +31,10 @@ form.addEventListener("submit", async (e) => {
         statusMessage.textContent = "❌ Es gab ein technisches Problem bitte versuche es miteinem anderen Browser oder im Inkoknito-Modus.";
         console.error(err);
     }
+
+ // Button wieder aktivieren nach 2 Minuten (120 Sekunden)
+    setTimeout(() => {
+        submitButton.disabled = false;
+    }, 180000);
+    
 });
